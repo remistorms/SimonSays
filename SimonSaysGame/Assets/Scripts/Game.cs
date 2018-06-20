04e6ba9fdaa6cd61c7aa2ac6af6bdae4e780eff0
REM_ <ultimate.spidey.18@gmail.com>
2018-06-20 18:16:13 -0500

    Game Script

---
+++ SimonSaysGame/Assets/Scripts/Game.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Game : MonoBehaviour {
+
+    public int score;
+	// Use this for initialization
+	void Start () {
+        GLOBAL.instance.M_event.EVT_Game_Setup += OnGameSetup;
+        GLOBAL.instance.M_event.EVT_Game_Start += OnGameStart;
+    }
+
+    void OnGameStart() {
+        Debug.Log("Game Script OnGameStart method has been called");
+    }
+
+    void OnGameSetup() {
+        score = 0;
+    }
+}
