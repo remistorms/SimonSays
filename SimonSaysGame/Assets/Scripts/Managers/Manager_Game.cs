14f2cbc8562021d2b4a714ac59bc239ddef89e8d
REM_ <ultimate.spidey.18@gmail.com>
2018-06-20 16:39:23 -0500

    New Manager Game Added
    Manager Game States

--- SimonSaysGame/Assets/Scripts/Managers/Manager_Game.cs
+++ SimonSaysGame/Assets/Scripts/Managers/Manager_Game.cs
@@ -4,14 +4,21 @@ using UnityEngine;
 
 public class Manager_Game : Manager {
 
+    public enum State {
+        None, 
+        Setup,
+        Start,
+        GameOver,
+
+    };
+
+    public State state;
+
     public override void Initialize()
     {
         base.Initialize();
-
+        state = State.None;
     }
 
-    public void DisplaySequenceToCopy() {
-
-        
-    }
+  
 }
