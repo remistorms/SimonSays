9f50ac9df14a64752dec22f39ffad2c06629ed0c
REM_ <ultimate.spidey.18@gmail.com>
2018-06-20 17:13:35 -0500

    Added Game States
    Game Over
    Setup
    Start

--- SimonSaysGame/Assets/Scripts/Managers/Manager_Game.cs
+++ SimonSaysGame/Assets/Scripts/Managers/Manager_Game.cs
@@ -14,14 +14,37 @@ public class Manager_Game : Manager {
 
     public State state;
 
+    private void Update()
+    {
+        //HACK
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Debug.Log("HACK has been called");
+            GLOBAL.instance.M_event.Fire_EVT_Game_Over();
+        }
+    }
+
     public override void Initialize()
     {
         state = State.None;
         Debug.Log("Initialized Manager Game");
         GLOBAL.instance.M_event.EVT_Game_Setup += OnGameSetup;
+        GLOBAL.instance.M_event.EVT_Game_Over += OnGameOver;
     }
 
     public void OnGameSetup() {
         Debug.Log("OnGameSetup Method has been called, thank you Events");
+        state = State.Setup;
+        StartCoroutine(DelayedStartRoutine());
+    }
+
+    public void OnGameOver() {
+        state = State.GameOver;
+    }
+
+    IEnumerator DelayedStartRoutine() {
+        yield return new WaitForSeconds(2);
+        GLOBAL.instance.M_event.Fire_EVT_Game_Start();
+        state = State.Start;
     }
 }
