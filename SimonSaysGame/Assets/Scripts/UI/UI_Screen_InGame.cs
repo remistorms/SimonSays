2adf76d93602325fbc9a10715e9aa59004fefc77
REM_ <ultimate.spidey.18@gmail.com>
2018-06-20 21:10:57 -0500

    Updated UI System
    UI_Screens and more

---
+++ SimonSaysGame/Assets/Scripts/UI/UI_Screen_InGame.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UI_Screen_InGame : UI_Screen {
+
+    public override Enum_Screen GetScreenType()
+    {
+        return Enum_Screen.InGame;
+    }
+}
---
+++ SimonSaysGame/Assets/Scripts/UI/UI_Screen_PostGame.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UI_Screen_PostGame : UI_Screen {
+
+    public override Enum_Screen GetScreenType()
+    {
+        return Enum_Screen.PostGame;
+    }
+
+}
