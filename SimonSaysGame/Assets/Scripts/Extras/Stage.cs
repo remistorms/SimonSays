190a7eb93ee016d3786e84d71825b2248f9223c6
REM_ <ultimate.spidey.18@gmail.com>
2018-06-21 18:42:58 -0500

    Finished Stage States
    Animations and Cool Stuff

--- SimonSaysGame/Assets/Scripts/Extras/Stage.cs
+++ SimonSaysGame/Assets/Scripts/Extras/Stage.cs
@@ -2,23 +2,65 @@
 using System.Collections.Generic;
 using UnityEngine;
 using DG.Tweening;
+using TMPro;
+using UnityEngine.UI;
 
 public class Stage : MonoBehaviour {
 
+    public enum State {
+        None,
+        Closed,
+        Open
+    };
+
+    public State stageState;
     public SkinnedMeshRenderer curtainsMesh;
+    public MeshRenderer outsideWallsMesh;
+
+    private void Start()
+    {
+        ResetStage();
+        GLOBAL.instance.M_event.EVT_Game_Setup += OnGameSetup;
+        GLOBAL.instance.M_event.EVT_Game_Over += OnGameOver;
+        GLOBAL.instance.M_event.EVT_Game_Start += OnGameStart;
+        GLOBAL.instance.M_event.EVT_Score_Changed += OnScoreChanged;
+    }
 
     private void Update()
     {
         if (Input.GetKeyDown(KeyCode.O))
         {
             OpenCurtains(1.5f);
+            FadeOutWalls(1.5f);
         }
 
         if (Input.GetKeyDown(KeyCode.C))
         {
             CloseCurtains(1.5f);
+            FadeInWalls(1.5f);
         }
     }
+
+    public void FadeOutWalls(float time) {
+        StartCoroutine(FadeOutWallsRoutine(time));
+    }
+
+    IEnumerator FadeOutWallsRoutine(float time) {
+        outsideWallsMesh.material.DOFade(0, time);
+        
+        yield return new WaitForSeconds(time);
+        outsideWallsMesh.gameObject.SetActive(true);
+       
+    }
+
+    public void FadeInWalls(float time)
+    {
+        outsideWallsMesh.gameObject.SetActive(true);
+        
+        outsideWallsMesh.material.DOFade(1, time);
+        
+    }
+
 
     public void OpenCurtains(float time) {
         Debug.Log("Opening Curtains");
@@ -48,5 +90,31 @@ public class Stage : MonoBehaviour {
     public void UpdateFloat(float value) {
 
         curtainsMesh.SetBlendShapeWeight(0, value);
+    }
+
+    public void ResetStage() {
+        stageState = State.Closed;
+        outsideWallsMesh.gameObject.SetActive(true);
+        outsideWallsMesh.material.color = new Color(outsideWallsMesh.material.color.r,
+            outsideWallsMesh.material.color.g,
+            outsideWallsMesh.material.color.b,
+            1f);
+        curtainsMesh.SetBlendShapeWeight(0, 0);
+    }
+
+    void OnGameSetup() {
+        ResetStage();
+    }
+
+    void OnGameOver() {
+
+    }
+
+    void OnGameStart() {
+
+    }
+
+    void OnScoreChanged(int score) {
+
     }
 }
