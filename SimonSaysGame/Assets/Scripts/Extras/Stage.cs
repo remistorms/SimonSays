5a8d60734d71d643f7f52294150cf36e05791180
REM_ <ultimate.spidey.18@gmail.com>
2018-06-22 12:45:45 -0500

    Game Loop New Art, Basic Functionality

--- SimonSaysGame/Assets/Scripts/Extras/Stage.cs
+++ SimonSaysGame/Assets/Scripts/Extras/Stage.cs
@@ -15,8 +15,8 @@ public class Stage : MonoBehaviour {
 
     public State stageState;
     public AudioSource audioSource;
-    public SkinnedMeshRenderer curtainsMesh;
-    public MeshRenderer outsideWallsMesh;
+    //public SkinnedMeshRenderer curtainsMesh;
+    public MeshRenderer outsideTrunk, insideTrunk;
     public CanvasGroup titleCanvasGroup;
 
     private void Start()
@@ -27,20 +27,21 @@ public class Stage : MonoBehaviour {
         GLOBAL.instance.M_event.EVT_Game_Start += OnGameStart;
         GLOBAL.instance.M_event.EVT_Score_Changed += OnScoreChanged;
         audioSource = GetComponent<AudioSource>();
+
     }
 
     private void Update()
     {
         if (Input.GetKeyDown(KeyCode.O))
         {
-            OpenCurtains(1.5f);
-            FadeOutWalls(1.5f);
+            //OpenCurtains(1.5f);
+            FadeOutWalls(1f);
         }
 
         if (Input.GetKeyDown(KeyCode.C))
         {
-            CloseCurtains(1.5f);
-            FadeInWalls(1.5f);
+            //CloseCurtains(1.5f);
+            FadeInWalls(1f);
         }
     }
 
@@ -49,24 +50,33 @@ public class Stage : MonoBehaviour {
     }
 
     IEnumerator FadeOutWallsRoutine(float time) {
-        outsideWallsMesh.material.DOFade(0, time);
-        
+        outsideTrunk.material.DOFade(0, time);
+        insideTrunk.gameObject.SetActive(true);
         yield return new WaitForSeconds(time);
-        outsideWallsMesh.gameObject.SetActive(true);
+        outsideTrunk.gameObject.SetActive(false);
        
     }
+    public void FadeInWalls(float time) {
+        StartCoroutine(FadeInWallsRoutine(time));
+        }
 
-    public void FadeInWalls(float time)
+    IEnumerator FadeInWallsRoutine(float time)
     {
-        outsideWallsMesh.gameObject.SetActive(true);
-        
-        outsideWallsMesh.material.DOFade(1, time);
-        
+        outsideTrunk.gameObject.SetActive(true);
+        outsideTrunk.material.color = new Color(outsideTrunk.material.color.r,
+            outsideTrunk.material.color.g,
+            outsideTrunk.material.color.b,
+            0f);
+        outsideTrunk.material.DOFade(1, time);
+        yield return new WaitForSeconds(time);
+        insideTrunk.gameObject.SetActive(false);
+             
     }
 
 
     public void OpenCurtains(float time) {
         Debug.Log("Opening Curtains");
+        insideTrunk.gameObject.SetActive(true);
         iTween.ValueTo(gameObject, iTween.Hash(
             "from", 0,
             "to", 100,
@@ -92,17 +102,18 @@ public class Stage : MonoBehaviour {
 
     public void UpdateFloat(float value) {
 
-        curtainsMesh.SetBlendShapeWeight(0, value);
+        //curtainsMesh.SetBlendShapeWeight(0, value);
     }
 
     public void ResetStage() {
         stageState = State.Closed;
-        outsideWallsMesh.gameObject.SetActive(true);
-        outsideWallsMesh.material.color = new Color(outsideWallsMesh.material.color.r,
-            outsideWallsMesh.material.color.g,
-            outsideWallsMesh.material.color.b,
+        outsideTrunk.gameObject.SetActive(true);
+        insideTrunk.gameObject.SetActive(false);
+        outsideTrunk.material.color = new Color(outsideTrunk.material.color.r,
+            outsideTrunk.material.color.g,
+            outsideTrunk.material.color.b,
             1f);
-        curtainsMesh.SetBlendShapeWeight(0, 0);
+       // curtainsMesh.SetBlendShapeWeight(0, 0);
         titleCanvasGroup.gameObject.SetActive(true);
         titleCanvasGroup.alpha = 1;
     }
---
+++ SimonSaysGame/Assets/Scripts/StartCubeButton.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StartCubeButton : MonoBehaviour {
+
+    BoxCollider boxCollider;
+
+    private void Start()
+    {
+        GLOBAL.instance.M_event.EVT_Game_Setup += OnGameSetup;
+        boxCollider = GetComponent<BoxCollider>();
+        boxCollider.enabled = true;
+
+    }
+    public void StartGameWithCubeCollider() {
+        GLOBAL.instance.M_event.Fire_EVT_Game_Setup();
+        boxCollider.enabled = false;
+    }
+
+    void OnGameSetup() {
+        boxCollider.enabled = true;
+    }
+
+}
